ed255cc0d50df71bdfd067de0e36cd40ff1a3fe3
kahlenden <nhatkhanh2k5@gmail.com>
2024-12-11 21:13:14 -0700

    re-configure librewolf

--- home-manager/librewolf.nix
+++ home-manager/librewolf.nix
@@ -5,30 +5,34 @@
 
     # https://librewolf.net/docs/settings/
     settings = {
-      # enable google safe browsing (locally-ish)
-      "browser.safebrowsing.malware.enabled" = true;
-      "browser.safebrowsing.phishing.enabled" = true;
-      "browser.safebrowsing.blockedURIs.enabled" = true;
-
-      "browser.safebrowsing.provider.google4.gethashURL" = "https://safebrowsing.googleapis.com/v4/fullHashes:find?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
-
-      "browser.safebrowsing.provider.google4.updateURL" =
-  "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
-
-      "browser.safebrowsing.provider.google.gethashURL" =
-  "https://safebrowsing.google.com/safebrowsing/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2";
-
-      "browser.safebrowsing.provider.google.updateURL" =
-  "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_SAFEBROWSING_API_KEY%";
-
-      "browser.safebrowsing.downloads.enabled" = true;
-
       # expanding or shrinking the inner window size in fixed increments, letting you blend in with a larger number of users
       "privacy.resistFingerprinting.letterboxing" = true;
 
-      # Preserve browsing and download history
-      "privacy.clearOnShutdown.history" = false;
-      "privacy.clearOnShutdown.downloads" = true;
+      # Do not preserve anything on shutdown
+      "privacy.clearOnShutdown.history"     = true;
+      "privacy.clearOnShutdown.downloads"   = true;
+      "privacy.clearOnShutdown.cache"       = true;
+      "privacy.clearOnShutdown.cookies"     = true;
+      "privacy.clearOnShutdown.formdata"    = true;
+      "privacy.clearOnShutdown.offlineApps" = true;
+      "privacy.clearOnShutdown.sessions"    = true;
+
+      # Blank start page. 0 = blank; 1 = home; 2 = last visited page; 3 = resume previous session;
+      "browser.startup.page" = 0;
+
+      # Disable os's geolocation services
+      "geo.provider.use_gpsd"     = false;
+      "geo.provider.use_geoclue"  = false;
+
+      # Disable search suggestion
+      "browser.search.suggest.enabled"  = false;
+      "browser.urlbar.suggest.engines"  = false;
+      "browser.urlbar.suggest.openpage" = false;
+      "browser.urlbar.suggest.topsites" = false;
+      "browser.urlbar.suggest.searches" = false;
+
+      # Disable bookmarks on toolbar
+      "browser.toolbars.bookmarks.visibility" = "never";
     };
   };
 
