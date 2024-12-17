9dbcc09859f75b4ed4324d60cb18bb37a69855ae
kahlenden <nhatkhanh2k5@gmail.com>
2024-12-16 20:02:57 -0700

    move printing service to hardware config

--- nixos/hardware/default.nix
+++ nixos/hardware/default.nix
@@ -4,11 +4,12 @@
     #./egpu-passthrough.nix
     ./audio.nix
     ./bluetooth.nix
+    ./cpu.nix
     ./graphics.nix
     ./hardware-configuration.nix
     ./network.nix
     ./openrazer.nix
+    ./printing.nix
     ./xserver.nix
-    ./cpu.nix
   ];
 }
